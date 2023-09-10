6e0cfe11246fa63270f7bd6a841194966d945a08
fanecovsf <gustavo_h07@hotmail.com>
2023-09-10 00:58:56 -0300

    ✨ Feat: Migrate para nome único na task

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class Task(models.Model):
 
 
     created_at = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100, blank=False, null=False)
+    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
     path = models.CharField(max_length=1000)
     description = models.CharField(max_length=2000, blank=True, null=True)
     schedule_param = models.CharField(max_length=50, blank=True, null=True)
